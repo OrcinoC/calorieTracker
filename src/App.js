57f5fe840b71488f95eb5ab6c23ca0045c4f1157
Colby Orcino <colbyorcino@Colbys-Air.lan>
2022-05-12 11:56:11 -0400

    reinitialized files

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import './App.css';
+import AppBar from './components/AppBar/AppBar';
+import AppControlsCounter from './components/AppControls/AppControlsCounter';
+import AppControlsDelete from './components/AppControls/AppControlsDelete';
+import AppControlsInputs from './components/AppControls/AppControlsInputs';
+import AppMealsList from "./components/AppMealsList/AppMealsList";
+import AppModal from "./components/AppModal/AppModal";
+
+const App = () => {
+  const[meals,setMeals] = useState([]);
+  const[mealName,setMealName] = useState("");
+  const[calories,setCalories] = useState(0);
+  const[openModal,setOpenModal] = useState(false);
+
+  const addMealsHandler = () => {
+    const oldMeals = [...meals];
+    const meal = {
+      mealName,
+      calories,
+      id: Math.floor(Math.random() * 1000),
+    };
+
+    const newMeals = oldMeals.concat(meal);
+
+    if(calories <= 0 || mealName === "") {
+      setOpenModal(true);
+    } else {
+      setMeals(newMeals);
+    }
+
+    setMealName("");
+    setCalories(0);
+  };
+
+  const deleteMealHandler = (id) => {
+    const oldMeals = [...meals];
+    const newMeals = oldMeals.filter((meal) => meal.id !== id);
+
+    setMeals(newMeals);
+  }
+
+  const total = meals.map((meal) => meal.calories).reduce((acc,value) => acc + +value, 0);
+
+  return (
+    <div className="App">
+      <AppBar />
+      <AppControlsCounter total={total}/>
+      
+      <AppControlsInputs addMealsHandler={addMealsHandler} mealName={mealName} calories={calories}
+        setMealName={setMealName} setCalories={setCalories}/>
+
+        <div className="app__meals__container">
+          <AppMealsList meals={meals} deleteMealHandler={deleteMealHandler} />
+        </div>
+    </div>
+  );
+}
+
+export default App;
